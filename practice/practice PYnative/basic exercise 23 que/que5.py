7af4bc927c303ef6216970ca659e0457eb8c14f6
Omkar Jadhav <omkarjadhav222004@gmail.com>
2025-08-21 09:04:04 +0530

    done

--- practice/practice PYnative/basic exercise 23 que/que5.py
+++ practice/practice PYnative/basic exercise 23 que/que5.py
@@ -1,2 +1,13 @@
 # Exercise 5: Check if the first and last numbers of a list are the same
 li=[]
+s=int(input("Enter a Size of List-"))
+print("Enter a element of list is:-")
+for i in range(s):
+    print((i+1),"element is:-")
+    e=int(input())
+    li.append(e)
+# print(li)
+if(li[0]==li[s-1]):
+    print("list frist and Last Element is same:-",li)
+else:
+    print("Frist and Last Element of List is not Same:-",li)
